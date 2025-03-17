26c7b46bac01e14ec85d2e01a64266249b48752f
Naman Saraswat <namaniisc@gmail.com>
2025-03-18 03:55:08 +0530

    Implement username prompt and validation
    Add logic in client.js to prompt users for a username
    Ensure that a valid username is entered before allowing any messages to be sent

--- public/client.js
+++ public/client.js
@@ -1,1 +1,11 @@
 const socket = io()
+
+let name;
+let textarea = document.querySelector('#textarea')
+let messageArea = document.querySelector('.message__area')
+
+do {
+    name = prompt('Please enter your name: ')
+} while(!name)
+
+
